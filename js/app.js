df80c671b8fab4fe2617d7e3b8c49b1a9a7b4aba
Anna <anuta.praga@gmail.com>
2016-11-28 12:36:26 +0200

    28.11.2016

--- js/app.js
+++ js/app.js
@@ -14,11 +14,7 @@ var my_news = [
         author: 'Shepperd',
         text: 'High priority',
         bigText: 'Task description'
-        // timeIn: 
-        // timeOut:
-        // total:
-    },
-    {
+    }, {
         author: 'Rami',
         text: 'High priority',
         bigText: "Task description"
@@ -30,7 +26,7 @@ window.ee = new EventEmitter();
 //--------------------------------------------------------------------------------------------------------------------------------------------------
 var Article = React.createClass({
     propTypes: {
-        data: React.PropTypes.shape ({
+        data: React.PropTypes.shape({
             author: React.PropTypes.string.isRequired,
             text: React.PropTypes.string.isRequired,
             bigText: React.PropTypes.string.isRequired
@@ -39,91 +35,130 @@ var Article = React.createClass({
 
     getInitialState: function() {
         return {
-            visible: false
-           
-            // clockInHide: false,
-            // clockOutHide: true,
-            // finishIsHide: true
+            visible: false,
+
+            clockInHide: false,
+            clockOutHide: true,
+            finishIsHide: true
         }
     },
-    readmoreClick: function (e){
+    readmoreClick: function(e) {
         e.preventDefault();
-        this.setState({visible: true});
+        this.setState({
+            visible: true
+        });
     },
-    onClockClick: function (){
+    onClockClick: function() {
         var time = new Date;
         this.props.data.timeIn = time.getTime();
-        console.log( this.props.data.author + ' : ' + this.props.data.timeIn); //начало отсчета
+        console.log(this.props.data.author + ' : ' + this.props.data.timeIn); //начало отсчета
+        this.setState({
+            clockOutHide: false
+        });
+        this.setState({
+            finishIsHide: false
+        });
+        this.setState({
+            clockInHide: true
+        });
     },
-    onClockOutClick: function(){
+    onClockOutClick: function() {
         var time = new Date;
         this.props.data.timeOut = time.getTime();
 
-        if (! this.props.data.timeIn) {
+        if (!this.props.data.timeIn) {
             alert('Task is not started');
         } else {
-            if(! this.props.data.totalTime) {
-                 this.props.data.totalTime = 0;
+            if (!this.props.data.totalTime) {
+                this.props.data.totalTime = 0;
             }
-                var interval = ( this.props.data.timeOut -  this.props.data.timeIn) / 60000;
-                this.props.data.totalTime += interval;
-        }
-            console.log(this.props.data.author + ' : ' + this.props.data.totalTime.toFixed(0));
+            var interval = (this.props.data.timeOut - this.props.data.timeIn) / 60000;
+            this.props.data.totalTime += interval;
+
             this.props.data.timeIn = ''; //обнулила начало отсчета
+            this.props.data.timeOut = '';
+
+            this.setState({
+                clockOutHide: true
+            });
+            this.setState({
+                finishIsHide: true
+            });
+            this.setState({
+                clockInHide: false
+            });
+        }
     },
-    onFinishClick: function(){
+    onFinishClick: function() {
+
+        console.log(this.props.data.author + ' : ' + this.props.data.totalTime.toFixed(0));
         this.props.data.timeIn = '';
         this.props.data.timeOut = '';
-        //обнулила счетчики
+        //обнулила счетчики и убрала кнопки
+        this.setState({
+            clockOutHide: true
+        });
+        this.setState({
+            finishIsHide: true
+        });
+        this.setState({
+            clockInHide: true
+        });
     },
 
-    render: function () {
+    render: function() {
         var author = this.props.data.author,
             text = this.props.data.text,
             bigText = this.props.data.bigText,
             visible = this.state.visible;
 
-            // var clockInHide = this.state.clockInHide,
-            //     clockOutHide = this.state.clockOutHide,
-            //     finishIsHide = this.state.finishIsHide;
+        var clockInHide = this.state.clockInHide,
+            clockOutHide = this.state.clockOutHide,
+            finishIsHide = this.state.finishIsHide;
 
-        return (
-            <div className= "article">
-                <p className= "news_author">{author}:</p>
-                <p className= "news_text">{text}</p>
+        return ( < div className = "article" >
+            < p className = "news_author" > {
+                author
+            }: < /p> < p className = "news_text" > {
+            text
+        } < /p>
 
-                {/* для ссылки readmore: не показывай ссылку, если visible === true */}
-                <a href="#" 
-                onClick = {this.readmoreClick}
-                className={"news_readmore" + (visible ? 'none': '') }>Description
-                </a>
+        { /* для ссылки readmore: не показывай ссылку, если visible === true */ } < a href = "#"
+        onClick = {
+            this.readmoreClick
+        }
+        className = {
+            "news_readmore" + (visible ? 'none' : '')
+        } > Description < /a>
 
-                {/* для большо текста: не показывай текст, если visible === false */}
-                <p className= {"news_bigText" + (visible ? '': 'none')}>{bigText}</p>
-                <button
-                    className= 'clock'
-                    onClick = {this.onClockClick}
-                    //disabled= {clockInHide}
-                     >
-                    Clock In
-                </button>
-                <button
-                    className= 'clock'
-                    onClick = {this.onClockOutClick}
-                    //disabled= {clockOutHide}
-                     >
-                    Clock Out
-                </button>
-                <button
-                    className= 'clock'
-                    onClick = {this.onFinishClick}
-                    //disabled= {finishIsHide}
-                     >
-                    Finished
-                </button>
-            </div>
-        )
-    }
+        { /* для большо текста: не показывай текст, если visible === false */ } < p className = {
+            "news_bigText" + (visible ? '' : 'none')
+        } > {
+            bigText
+        } < /p> < button className = 'clock'
+        onClick = {
+            this.onClockClick
+        }
+        disabled = {
+                clockInHide
+            } >
+            Clock In < /button> < button className = 'clock'
+        onClick = {
+            this.onClockOutClick
+        }
+        disabled = {
+                clockOutHide
+            } >
+            Clock Out < /button> < button className = 'clock'
+        onClick = {
+            this.onFinishClick
+        }
+        disabled = {
+                finishIsHide
+            } >
+            Finished < /button> < /div >
+    )
+}
 })
 
 //-----------------------------------------------------------------------------------------------------------------------------------
@@ -143,25 +178,28 @@ var News = React.createClass({
 
         if (data.length > 0) {
             newsTemplate = data.map(function(item, index) {
-                return (
-                    <div key={index}>
-                        <Article data= {item}/>
-                    </div>
+                return ( < div key = {
+                        index
+                    } >
+                    < Article data = {
+                        item
+                    }
+                    /> < /div >
                 )
             })
         } else {
-            newsTemplate = <p>:) we haven't any tasks</p>
-        }
-        return (
-            <div className="news">
-                {newsTemplate}
-                <strong
-                className={'news_count '+ (data.length > 0 ? '':'none')}>
-                Quantity of tasks: {data.length}
-                </strong>
-            </div>
-        );
+            newsTemplate = < p > : ) we haven 't any tasks</p>
     }
+    return ( < div className = "news" > {
+            newsTemplate
+        } < strong className = {
+            'news_count ' + (data.length > 0 ? '' : 'none')
+        } >
+        Quantity of tasks: {
+            data.length
+        } < /strong> < /div >
+    );
+}
 });
 
 //----------------------------------------------------------------------------------------------------------------------------------------
@@ -174,39 +212,47 @@ var Add = React.createClass({
             textIsEmpty: true
         };
     },
-    componentDidMount: function(){
+    componentDidMount: function() {
         ReactDOM.findDOMNode(this.refs.author).focus();
     },
     onButtonClickHandler: function(e) {
-       e.preventDefault();
-       var textEl = ReactDOM.findDOMNode(this.refs.bigText);
-       var bigText = textEl.value;
-       var auth = ReactDOM.findDOMNode(this.refs.author);
-       var author = auth.value;
-       var priority = ReactDOM.findDOMNode(this.refs.text);
-       var text = priority.value;
-       
-       var item = [{
+        e.preventDefault();
+        var textEl = ReactDOM.findDOMNode(this.refs.bigText);
+        var bigText = textEl.value;
+        var auth = ReactDOM.findDOMNode(this.refs.author);
+        var author = auth.value;
+        var priority = ReactDOM.findDOMNode(this.refs.text);
+        var text = priority.value;
+
+        var item = [{
             author: author,
             text: text,
             bigText: bigText,
             totalTime: ''
         }];
-       window.ee.emit('News.add', item);
-       textEl.value = '';
-       priority.value = '';
-       auth.value = '';
-       this.setState({bigTextIsEmpty: true});
+        window.ee.emit('News.add', item);
+        textEl.value = '';
+        priority.value = '';
+        auth.value = '';
+        this.setState({
+            bigTextIsEmpty: true
+        });
     },
     onCheckRuleClick: function(e) {
-        this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+        this.setState({
+            agreeNotChecked: !this.state.agreeNotChecked
+        });
     },
-   
+
     onFieldChange: function(fieldName, e) {
         if (e.target.value.trim().length > 0) {
-            this.setState({['' + fieldName] : false})
+            this.setState({
+                ['' + fieldName]: false
+            })
         } else {
-             this.setState({['' + fieldName] : true})
+            this.setState({
+                ['' + fieldName]: true
+            })
         }
     },
     render: function() {
@@ -214,42 +260,44 @@ var Add = React.createClass({
             authorIsEmpty = this.state.authorIsEmpty,
             textIsEmpty = this.state.textIsEmpty,
             bigTextIsEmpty = this.state.bigTextIsEmpty;
-        return (
-        <form className = 'addForm'>
-           <input
-                type= 'text'
-                className= 'add_author'
-                onChange = {this.onFieldChange.bind(this, 'authorIsEmpty')}
-                placeholder= 'Name'
-                ref= 'author'
-                />
-           <input
-                type= 'text'
-                className= 'add_text'
-                onChange = {this.onFieldChange.bind(this, 'textIsEmpty')}
-                placeholder= 'Priority'
-                ref= 'text'
-                />
-           <textarea
-                className= 'add_bigText'
-                onChange = {this.onFieldChange.bind(this, 'bigTextIsEmpty')}
-                placeholder= 'Task description'
-                ref= 'bigText' >
-            </textarea>
-            <label className= 'add_checkrule'>
-                <input type= 'checkbox' ref= 'checkrule' onChange= {
-                   this.onCheckRuleClick} />
-                I'm sure
-             </label>
+        return ( < form className = 'add cf' >
+            < input type = 'text'
+            className = 'add_author'
+            onChange = {
+                this.onFieldChange.bind(this, 'authorIsEmpty')
+            }
+            placeholder = 'Name'
+            ref = 'author' / >
+            < input type = 'text'
+            className = 'add_text'
+            onChange = {
+                this.onFieldChange.bind(this, 'textIsEmpty')
+            }
+            placeholder = 'Priority'
+            ref = 'text' / >
+            < textarea className = 'add_bigText'
+            onChange = {
+                this.onFieldChange.bind(this, 'bigTextIsEmpty')
+            }
+            placeholder = 'Task description'
+            ref = 'bigText' >
+            < /textarea> < label className = 'add_checkrule' > < input type = 'checkbox'
+            ref = 'checkrule'
+            onChange = {
+                this.onCheckRuleClick
+            }
+            />
+            I 'm sure < /label>
 
-            <button
-                className= 'add_btn'
-                onClick= {this.onButtonClickHandler}
-                ref= 'add_button'
-                disabled= {agreeNotChecked || authorIsEmpty || textIsEmpty || bigTextIsEmpty} >
-                    Add task
-            </button>
-         </form>
+            < button className = 'add_btn'
+            onClick = {
+                this.onButtonClickHandler
+            }
+            ref = 'add_button'
+            disabled = {
+                agreeNotChecked || authorIsEmpty || textIsEmpty || bigTextIsEmpty
+            } >
+            Add task < /button> < /form >
         );
     }
 });
@@ -267,7 +315,9 @@ var App = React.createClass({
         var self = this;
         window.ee.addListener('News.add', function(item) {
             var nextNews = item.concat(self.state.news);
-            self.setState({news: nextNews});
+            self.setState({
+                news: nextNews
+            });
         });
     },
 
@@ -277,17 +327,15 @@ var App = React.createClass({
     },
     render: function() {
         console.log('render');
-        return (
-            <div className="app">
-                <h3>TASKS</h3>
-                <Add /> {/*Добавила вывод компонента*/}
-                <News data={this.state.news} />
-            </div>
+        return ( < div className = "app" >
+            < h3 > TASKS < /h3> < Add / > { /*Добавила вывод компонента*/ } < News data = {
+                this.state.news
+            }
+            /> < /div >
         );
     }
 });
 
-ReactDOM.render(
-    <App />,
+ReactDOM.render( < App / > ,
     document.getElementById('root')
 );
